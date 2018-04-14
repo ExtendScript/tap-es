ce5505204cb5c9820741041bd3a9ef4ad38f963e
Bruno Herfst <hello@brunoherfst.com>
2018-04-15 00:26:52 +1000

    outline
    Still need to resolve the runner logic

--- README.md
+++ README.md
@@ -1,30 +1,50 @@
 # tap-es
 
-Tap-extendscript is a utiliy wrapper arround [estktap](https://github.com/ExtendScript/estktap) and [tap-markdown](https://github.com/Hypercubed/tap-markdown).
+Tap-ExtendScript is a utiliy wrapper arround [estktap](https://github.com/ExtendScript/estktap) and [tap-markdown](https://github.com/Hypercubed/tap-markdown). It creates a tape test bundler, generator and reporter that takes a test (or a set of tests) and pipes them to multiple app targets/versions. The results are saved to a single Markdown report.
 
-## goals
+## install
 
-  - Run multiple tests targeting multiple apps and versions
-  - Generate a single mark-down report
+    npm install tap-es --save-dev
 
-## usage
+## use cli
 
-### CLI
+This is handy for testing scripts using `npm run` inside your package.json
 
-    tap-es -s ./script.jsx -o ./report.md indesign-13 photoshop-18
+    tap-es -s ./test/*.jsx -o ./report.md indesign-13 photoshop-18
 
-### As a module
-
-    require(tap-es)
-    
-    tap-es.addTest({
-    	scriptPaths: [pathToScript1.jsx,pathToScript2.jsx],
-    	targets: [indesign-12, photoshop-18],
-    	comparitor: true,
-    	reportPath: pathToReport.md
-    })
-    
-    tap-es.run()
+> Use `tape-es -h` for help
 
 
-Please read the [estktap guide](https://github.com/nbqx/estktap#readme) on how to set up tests.
+## use as module
+
+Using as a module you have fine grain control over the setup and bundling of your tests.
+
+### adding and generating tests
+
+The `add` function is a powerfull tool that generates a test for each script to each target. When we need more fine control to define which scripts target specific comparators we can run the generator multiple times with different settings before evoking the `run` command.
+
+The `add` function takes three arguments:
+
+  1. __test__ `String`, `Array`: Path to script files to test
+  2. __target__ `String`, `Array`: Adobe app targets and version
+  3. __comparator__ `Number`, `String`, `Boolean` or `Function` [Optional, defaults to `true`]
+
+Example using multiple files and multiple targets:
+
+    // Generates 6 tests
+    tap-es.add(['test1.jsx','test2.jsx'], [indesign-12,indesign-13,photoshop-18])
+
+We can also pass [glob](https://github.com/isaacs/node-glob) strings:
+
+    tap-es.add('./test/*.jsx', [indesign-12,indesign-13] )
+
+> Please read the [estktap guide](https://github.com/nbqx/estktap#readme) on how to set up test files.
+
+
+### Running the tests
+
+Now we have created the tests, run them and save output to markdown and console:
+
+    tap-es.run( './result.md' )
+
+> Running a test also resets tap-es
--- tap-es.js
+++ tap-es.js
@@ -1,5 +1,147 @@
-//var shell   = require('shelljs');
-//var cmd = ['tape', './lib/weaver.js', '-s', args.script, '-c', args.comparitor, targets.join(" "), "| tap-markdown"].join(" ");
+// The tapestry
 
-//shell.exec(cmd)
-//     .to(args.report);
+/*
+  A helper function to clean array reference
+  -----
+  Param arr : Array, String
+
+*/
+function newStringArray( arr ) {
+  // Clean array reference and make sure tests is type of array
+  return (Array.isArray(arr)) ? arr.splice(0) : [String(arr)];
+};
+
+/*
+  A helper function to clean array values by constructor
+  -----
+  Param inst : Instance
+  Param arr  : Array
+
+*/
+function keepInstanceOf( inst, arr ) {
+  // Remove all values that are not instance of param inst
+  if ( !Array.isArray(arr) ) return [];
+
+  var i = arr.length;
+  while (i--) {
+    if (! arr[i] instanceof inst) { 
+      arr.splice(i, 1);
+    };
+  };
+  return arr;
+};
+
+/*
+  Single Test
+  -----
+  Param tests      : String: Path to script
+  Param targets    : Array, String: [list of] Adobe application target
+  Param comparator : Number, String, Boolean, Function: Test comparator
+
+*/
+function Test( tests, targets, comparator ) {
+  var _Test = this;
+  // Object must be created with new
+  if( !(_Test instanceof Test) ){
+    return new Test( tests, targets, comparator );
+  };
+
+  // String: Path to JSX script
+  _Test.tests: newStringArray( tests );
+
+  // Array: of target strings
+  _Test.targets = newStringArray( targets );
+
+  // Parse test comparator
+  switch (typeof comparator) {
+    case 'string':
+    case 'number':
+    case 'boolean':
+    case 'function':
+      _Test.comparator = comparator;
+      break;
+    default:
+      _Test.comparator = true;
+      break;
+  };
+};
+
+/*
+  The Deck holds all tests that need to be taped
+  -----
+  Param tests      : Array of objects (Test)
+  Param resultPath : String: Path to export file (eg /path/to/results.md)
+
+*/
+function Deck( tests ) {
+  var _Deck = this;
+  // Object must be created with new
+  if( !(_Deck instanceof Deck) ){
+    return new Deck( tests, resultPath );
+  };
+  
+  // Array: Containing tests
+  _Deck.tests = new Array();
+
+  _Deck.add = function( tests ) {
+    if ( Array.isArray(tests) ){
+      _Deck.tests.concat(keepInstanceOf( Test, tests.slice(0) ));
+    } else if(tests instanceof Test) {
+      _Deck.tests.push(tests);
+    };
+  };
+  
+  _Deck.get = function() {
+    return _Deck.tests.slice(0);
+  };
+
+  _Deck.reset = function() {
+    _Deck.tests = new Array();
+  };
+
+  _Deck.add( tests );
+};
+
+//----------------------------------------------------------------------
+var exports = {};
+//----------------------------------------------------------------------
+
+var deck = exports.deck = new Deck();
+
+var reset = exports.reset = function() {
+  deck.reset();
+};
+
+
+/*
+  Function: Test Generator
+  -----
+  Param tests      : String, Array: [list of] Path to script, or glob path
+  Param targets    : Array, String: [list of] Adobe application target
+  Param comparator : Number, String, Boolean, Function: Test comparator
+
+*/
+var add = exports.add = function( tests, targets, comparator) {
+  deck.add( new Test( tests, targets, comparator ) );
+};
+
+/*
+  Function: Test Runner
+  -----
+  Param output:  String: Path to export file (eg /path/to/results.md)
+
+*/
+var run = exports.run = function( output ) {
+  var R = this;
+  // String: Path to results.md
+  R.output = String( output );
+  R.tests = deck.get();
+  
+  console.log("____________________________ EXPORT! " + R.tests + " ____________________________")
+
+  deck.reset();
+};
+
+//----------------------------------------------------------------------
+module.exports = exports;
+//----------------------------------------------------------------------
